89a608f46e534486fb2e1123ac858f5e0f20a166
Leonardo Rodrigues Miranda Lins <leonardolins.ext@hotmail.com>
2020-01-05 15:45:11 -0300

    Done beta version!

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -16,18 +16,35 @@ export default class Converter extends Component {
 
     convert(){
         let sentence = `${this.props.currencyA}_${this.props.currencyB}`; 
-        let url = `http://free.currencyconverterapi.com/api/v5/convert?q=${sentence}`
+        let url = `https://free.currconv.com/api/v7/convert?q=${sentence}&compact=ultra&apiKey=1b7a6ebc052541a5deea`
+
+        fetch(url)
+        .then(res=>{
+            return res.json();
+        })
+        .then(cont=>{
+            let price = cont[sentence];
+            let target = this.state.currencyA_value
+            let result = (parseFloat(target)*price).toFixed(2);
+            console.log(result);
+            this.setState({currencyB_value: result});
+        })
     }
 
     render(){
         return (
             <div className="Converter">
                 <h2>{this.props.currencyA} to {this.props.currencyB}</h2>
-                <input type="input"></input>
-                <div>
-                    <input type="button" value="Converter"></input>
+                <input type="input"
+                    onChange={(event)=>{this.setState({ currencyA_value: event.target.value })}} ></input>
+                <div className="Button">
+                    <input type="button"
+                        value="Converter"
+                        onClick={this.convert} ></input>
                 </div>
-                <p></p>
+                <div className="Result">
+                    <h3>R$ {this.state.currencyB_value}</h3>
+                </div>
             </div>
         )
     }
